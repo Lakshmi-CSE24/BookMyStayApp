d6a1e6052cf17d12cdf626ead09315c85aada10a
Lakshmi-CSE24 <lakshmisaraswathi2006@gmail.com>
2026-03-03 16:26:21 +0530

    UC1: Application Entry & Welcome Message

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,2 +1,28 @@
+/**
+ * HotelBookingApp
+ *
+ * Entry point of the Book My Stay Application.
+ * Demonstrates application startup and console output.
+ *
+ * @author Rishika
+ * @version 1.0
+ */
+
 public class HotelBookingApp {
+
+    /**
+     * Application entry point.
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("============================================");
+        System.out.println(" Welcome to the Hotel Booking Management System ");
+        System.out.println("============================================");
+
+        System.out.println("Application Name : Book My Stay App");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+
+    }
 }
